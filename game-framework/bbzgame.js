5e104f52b04d27e77ef083b4b2269ad0cf7c6bf4
Nicolas Müller <nicolas.mueller2@gmail.com>
2026-04-17 23:05:23 +0200

    Updates

--- game-framework/bbzgame.js
+++ game-framework/bbzgame.js
@@ -1,34 +1,32 @@
 // Expose the framework as a classic browser script so it can be loaded from GitHub Pages.
 (function exposeBBZGame(global) {
-  function initGame() {
-    console.log("Initializing BBZGame Script...");
+    function initGame() {
+        console.log("Initializing BBZGame Script...");
 
-    const playground = document.querySelector(".playground");
+        const playground = document.querySelector("#playground");
 
-    if (playground) {
-      playground.style.position = "relative";
+        if (playground) {
+            playground.style.position = "relative";
+        }
+
+        const gameObjects = document.querySelectorAll(".gameobject, #player");
+
+        gameObjects.forEach((obj) => {
+            obj.style.position = "absolute";
+        });
     }
 
-    const gameObjects = document.querySelectorAll(".gameobject, #player");
+    function setPosition(element, x, y) {
+        element.style.left = `${x}px`;
+        element.style.bottom = `${y}px`;
+    }
 
-    gameObjects.forEach((obj) => {
-      obj.style.position = "absolute";
-    });
-  }
+    global.initBBZGame = initGame;
+    global.setPosition = setPosition;
 
-  function setPosition(element, x, y) {
-    element.style.left = `${x}px`;
-    element.style.bottom = `${y}px`;
-  }
-
-  global.BBZGame = {
-    initGame,
-    setPosition,
-  };
-
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initGame, { once: true });
-  } else {
-    initGame();
-  }
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", initGame, { once: true });
+    } else {
+        initGame();
+    }
 })(window);
